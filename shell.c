ff7dda71bb9a002608c58d66fbfe950ea4640e53
ntnfolla <ntnfolla@gmail.com>
2022-06-15 15:17:07 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,49 +1,48 @@
 #include "shell.h"
 /**
- * main - reads commands and executes
- * @ac: number of args
- * @av: pointer to args
- * @envp: pointer to env variables
+ * main - carries out the read, execute then print output loop
+ * @ac: argument count
+ * @av: argument vector
+ * @envp: environment vector
+ *
  * Return: 0
  */
-
 int main(int ac, char **av, char *envp[])
 {
-char *line = NULL, *command_args = NULL, *path = NULL;
+char *line = NULL, *pathcommand = NULL, *path = NULL;
 size_t bufsize = 0;
-ssize_t lsize = 0;
-char **command = NULL, **path_args = NULL;
+ssize_t linesize = 0;
+char **command = NULL, **paths = NULL;
 (void)envp, (void)av;
-
 if (ac < 1)
 return (-1);
-signal(SIGINT, test_signal);
+signal(SIGINT, handle_signal);
 while (1)
 {
-free_buff(command);
-free_buff(path_args);
-free(command_args);
-prompt();
-lsize = getline(&line, &bufsize, stdin);
-if (lsize < 0)
+free_buffers(command);
+free_buffers(paths);
+free(pathcommand);
+prompt_user();
+linesize = getline(&line, &bufsize, stdin);
+if (linesize < 0)
 break;
 info.ln_count++;
-if (line[lsize - 1] == '\n')
-line[lsize - 1] = '\0';
-command = token(line);
+if (line[linesize - 1] == '\n')
+line[linesize - 1] = '\0';
+command = tokenizer(line);
 if (command == NULL || *command == NULL || **command == '\0')
 continue;
-if (check(command, line))
+if (checker(command, line))
 continue;
-path = get_path();
-path_args = token(path);
-command_args = check_path(path_args, command[0]);
-if (!command_args)
+path = find_path();
+paths = tokenizer(path);
+pathcommand = test_path(paths, command[0]);
+if (!pathcommand)
 perror(av[0]);
 else
-execute(command_args, command);
+execution(pathcommand, command);
 }
-if (lsize < 0 && flags.interactive)
+if (linesize < 0 && flags.interactive)
 write(STDERR_FILENO, "\n", 1);
 free(line);
 return (0);
